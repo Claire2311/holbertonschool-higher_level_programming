6af70d74d32bded15747e0fe7e9c9c4fa07e354c
Claire2311 <claire.girard@ieseg.fr>
2024-03-29 16:47:26 +0100

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,10 +15,3 @@ def add_integer(a, b=98):
     if not isinstance(b, int):
         raise TypeError('b must be an integer')
     return a + b
-
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-print(add_integer('cool', -2))
