f28db4d0a598c6a34287e591b1e8f3816677f61e
Claire2311 <claire.girard@ieseg.fr>
2024-03-29 13:42:42 +0100

    Task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Class that defines a square"""
+
+
+class Square:
+    """the class to define a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        self.__size = size
+        if not isinstance(size, int):
+            raise ValueError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        return self.__size * self.__size
+    
+    def my_print(self):
+        for i in range(1, self.__size + 1):
+            print('#' * self.__size)
