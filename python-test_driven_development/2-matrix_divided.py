a2a95307ba51931fd62a8c3bc11f6dc23fda15ef
Claire2311 <claire.girard@ieseg.fr>
2024-03-30 15:03:25 +0100

    Task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -34,7 +34,8 @@ def matrix_divided(matrix, div):
             raise TypeError('Each row of the matrix must have the same size')
 
     if not isinstance(div, (int, float)):
-        raise TypeError()
+        raise TypeError('div must be a number')
+
     if div == 0:
         raise ZeroDivisionError('division by zero')
 
