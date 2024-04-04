f0ed3026cb51f11dc988556a219d8369ae31ada9
Claire2311 <claire.girard@ieseg.fr>
2024-04-04 22:21:49 +0200

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- script that creates the database hbtn_0c_0
 DROP DATABASE IF EXISTS `second_table`;
 
-CREATE TABLE first_table (
+CREATE TABLE `second_table` (
     id INT NOT NULL, name VARCHAR(256), score INT
-)
+);
 
 INSERT INTO
     `second_table` (id, name, score)
