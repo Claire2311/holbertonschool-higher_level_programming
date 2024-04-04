6b44d0cd430578318cfc803af4a2aed6eb8c2f69
Claire2311 <claire.girard@ieseg.fr>
2024-04-04 23:23:35 +0200

    Task 17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- script that lists all records of the table second_table
 
-USE hhbtn_0c_0;
+USE hbtn_0c_0;
 
 ALTER TABLE first_table
 MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
