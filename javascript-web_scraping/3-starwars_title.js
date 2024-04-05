1201acc44c96bd43e231c59daeadaaaf487e9e31
Claire2311 <claire.girard@ieseg.fr>
2024-04-05 17:22:46 +0200

    Task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,9 @@ const request = require('request');
 request(
   'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
   function (err, res, body) {
+    if (err) {
+      return console.log(err);
+    }
     const result = JSON.parse(body);
     console.log(result.title);
   }
