4b44bf46a4ba7c82896853c7e81e5e552581cc75
Claire2311 <claire.girard@ieseg.fr>
2024-04-05 15:46:15 +0200

    Task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,7 @@
+const request = require("request");
+
+request(url, function (error, response, body) {
+  console.error("error:", error);
+  console.log("statusCode:", response && response.statusCode);
+  return body;
+});
---
+++ javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+Exercices about JavaScript - Web scraping
