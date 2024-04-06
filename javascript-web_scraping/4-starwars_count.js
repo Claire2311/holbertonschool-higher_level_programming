b95861573e052fc04366014f8f16c99752cbf815
Claire2311 <claire.girard@ieseg.fr>
2024-04-06 15:56:10 +0200

    Task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request(process.argv[2], function (err, res, body) {
+request('https://swapi-api.hbtn.io/api/films/', function (err, res, body) {
   if (err) {
     return console.log(err);
   }
@@ -11,9 +11,9 @@ request(process.argv[2], function (err, res, body) {
   let numChar = 0;
 
   let i = 0;
-  let j = 0;
 
   for (i; i < result.results.length; i++) {
+    let j = 0;
     for (j; j < result.results[i].characters.length; j++) {
       if (result.results[i].characters[j].includes(18)) {
         numChar += 1;
